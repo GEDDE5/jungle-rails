efb5acff753b9176b9ca22432f0b82afc4448bb5
GEDDE5 <geddes.3754@gmail.com>
2017-03-14 18:59:16 -0700

    Cleans up line 33

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -30,9 +30,7 @@ class Admin::CategoriesController < ApplicationController
   private
 
   def category_params
-    params.require(:category).permit(
-      :name,
-    )
+    params.require(:category).permit(:name)
   end
 
 end
