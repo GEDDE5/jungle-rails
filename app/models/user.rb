e7ef2c368825d14f3416936572268b7b0ec8c38c
GEDDE5 <geddes.3754@gmail.com>
2017-03-15 23:59:38 -0700

    Adds has_many/belongs_to relationships to pertinent models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
 
   has_secure_password
+  # has_many :reviews
 
   validates :first_name, presence: { message: "A first name must be specified" }, length: { minimum: 3, message: "must be at least (3) characters long" }
   validates :last_name, presence: { message: "A last name must be specified" }, length: { minimum: 3, message: "must be at least (3) characters long" }
