ff76c5c3cd04308390f08cf7d746f07f7715aa66
GEDDE5 <geddes.3754@gmail.com>
2017-03-17 16:37:49 -0700

    Adds helper function for reviews.user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,15 @@
 class User < ActiveRecord::Base
 
   has_secure_password
-  # has_many :reviews
+  has_many :reviews
 
   validates :first_name, presence: { message: "A first name must be specified" }, length: { minimum: 3, message: "must be at least (3) characters long" }
   validates :last_name, presence: { message: "A last name must be specified" }, length: { minimum: 3, message: "must be at least (3) characters long" }
   validates :email, presence: { message: "An email must be specified" }, length: { minimum: 5, message: "must be at least (5) characters long" }, uniqueness: { message: "must be unique"}
   validates :password, presence: { message: "A password must be specified" }, length: { in: 6..20, message: "must be between 6-20 characters long" }
+
+  def has_reviews?
+    self.reviews.count > 0
+  end
 
 end
