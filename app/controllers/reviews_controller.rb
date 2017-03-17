135085d06bb344677bfbf0fbd5e815812adfcdcb
GEDDE5 <geddes.3754@gmail.com>
2017-03-16 19:11:53 -0700

    Protect reviews_controller via before_filter

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,15 @@
 class ReviewsController < ApplicationController
+  before_filter :require_login
+
   def create
     product = Product.find_by(id: params[:product_id])
     @review = product.reviews.new(review_params)
     @review.user_id = current_user.id
 
     if @review.save
-      redirect_to product_path(id: params[:product_id]), notice: "Review successfully submitted"
+      redirect_to path_to_product, notice: "Review successfully submitted"
     else
-      redirect_to product_path(id: params[:product_id]), notice: "Review incomplete: please choose a rating"
+      redirect_to path_to_product, notice: "Review incomplete: please choose a rating"
     end
   end
 
@@ -17,8 +19,16 @@ class ReviewsController < ApplicationController
     redirect_to product_path(id: params[:product_id])
   end
 
+  def path_to_product
+    product_path(id: params[:product_id])
+  end
+
   private
   def review_params
     params.require(:review).permit(:rating, :description, :product_id, :user_id)
+  end
+
+  def require_login
+    redirect_to path_to_product unless current_user
   end
 end
